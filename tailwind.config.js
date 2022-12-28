0f77ed0c401731686b78206706a4e46463db2f60
hasanchamdany <hasanchamdany@gmail.com>
2022-12-29 00:55:54 +0700

    try floating navbar

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        current: "currentColor",
+        's-orange': '#E16A3D',
+        't-orange': '#FFA45D',
+        't-blue':'#016A6D',
+        'n-blue':'#043E52'
+      },
+    },
+  },
+  plugins: [],
+}
