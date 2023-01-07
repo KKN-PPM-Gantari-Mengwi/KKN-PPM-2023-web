9bfb4a001858eaa277bfa9de163c5540619ef48f
hasanchamdany <hasanchamdany@gmail.com>
2023-01-07 10:56:48 +0700

    feat(anime): add home animation effect

--- src/components/Card/HomeCard.jsx
+++ src/components/Card/HomeCard.jsx
@@ -3,7 +3,10 @@ import Image from "next/image";
 
 const HomeCard = (props) => {
   return (
-    <div className="lg:w-[580px] md:w-[500px] sm:w-[450px] w-[300px] md:h-[646px] h-[450px] bg-gradient-to-b from-[#043E52] to-[#016A6D] rounded-[45px]">
+    <div
+      className="2xl:w-[580px] xl:w-[500px] sm:w-[450px] w-[300px] 2xl:h-[646px] xl:h-[600px] h-[450px] bg-gradient-to-b from-[#043E52] to-[#016A6D] rounded-[45px]"
+      data-aos={props.anime}
+    >
       {/* image card */}
       <div className="homeCardCover max-w-[519px] md:h-[372px] h-[250px] mx-auto block">
         <Image
@@ -13,20 +16,23 @@ const HomeCard = (props) => {
           height={372}
           layout={"responsive"}
           alt="desa-baha"
-          className="mx-auto mt-6"
+          className="2xl:px-0 xl:px-4 2xl:mt-6 xl:mt-4"
         />
       </div>
       <div className="md:mt-4 mt-1">
-        <div className="mx-auto">
-          <h3 className="flex justify-center text-white text-5xl font-bold font-Natural">
+        <div className="mx-auto" >
+          <h3 className="flex justify-center text-white text-5xl font-bold font-Natural" data-aos="fade-up">
             Desa
           </h3>
-          <h3 className="flex justify-center textGradOrange text-4xl font-bold font-poppins bg-gradient-to-b from-s-orange to-t-orange">
+          <h3 className="flex justify-center textGradOrange text-4xl font-bold font-poppins bg-gradient-to-b from-s-orange to-t-orange" data-aos="fade-up">
             {props.desa}
           </h3>
         </div>
       </div>
-      <div className="flex mx-auto max-w-[275px] h-[45px] mt-8 bg-gradient-to-r from-s-orange to-t-orange items-center justify-center rounded-[30px] font-poppins text-xl text-white font-semibold">
+      <div
+        className="flex mx-auto max-w-[275px] h-[45px] mt-8 bg-gradient-to-r from-s-orange to-t-orange items-center justify-center rounded-[30px] font-poppins text-xl text-white font-semibold"
+        data-aos="fade-up"
+      >
         Selengkapnya
       </div>
     </div>
--- src/pages/index.js
+++ src/pages/index.js
@@ -13,20 +13,32 @@ export default function Home() {
       <section className="bg-home-1 h-screen w-full">
         {/* <Navbar/> */}
         <div className="homeCover1  w-full h-full">
-          <div className="md:pt-[430px] pt-[250px]">
+          <div className="2xl:pt-[340px] pt-[250px]">
             <div className="block z-0">
-              <h1 className="flex justify-center items-center text-t-orange my-auto -translate-x-5  md:text-8xl text-6xl font-Natural z-0">
+              <h1
+                className="flex justify-center items-center text-t-orange my-auto -translate-x-5  md:text-9xl text-6xl font-Natural z-0"
+                data-aos="fade-up"
+              >
                 Pesona
               </h1>
-              <h1 className="flex justify-center items-center text-white my-auto translate-x-5 -translate-y-3 md:text-8xl text-6xl font-Natural">
+              <h1
+                className="flex justify-center items-center text-white my-auto translate-x-5 -translate-y-3 md:text-9xl text-6xl font-Natural"
+                data-aos="fade-up"
+              >
                 Mengwi
               </h1>
-              <h3 className="flex justify-center items-center text-white pt-2 text-xl font-poppins">
+              <h3
+                className="flex justify-center items-center text-white pt-2 text-xl font-poppins"
+                data-aos="fade-up"
+              >
                 KKN PPM UGM Periode 2 Tahun 2023
               </h3>
             </div>
 
-            <div className="mt-2 mx-auto h-[40px] w-[364px] rounded-[30px] bg-gradient-to-r from-s-orange to-t-orange">
+            <div
+              className="mt-2 mx-auto h-[40px] w-[364px] rounded-[30px] bg-gradient-to-r from-s-orange to-t-orange"
+              data-aos="fade-up"
+            >
               <p className="flex justify-center items-center pt-2 text-white font-poppins">
                 Jelajahi Sekarang!
               </p>
@@ -35,12 +47,18 @@ export default function Home() {
         </div>
       </section>
       <section className="flex bg-home-2 w-full h-screen">
-        <div className="w-[1537px] lg:h-[639px] h-fit  mx-auto my-auto lg:mt-48 mt-20 bg-gradient-to-r from-n-blue to-t-blue rounded-[30px]">
-          <div className="flex justify-center text-6xl text-white font-Natural pt-12 ">
+        <div className="2xl:w-[1537px] xl:w-[1300px] 2xl:h-[639px]  xl:h-[600px] sm:-[700px]  mx-auto my-auto 2xl:mt-48 mt-20 bg-gradient-to-r from-n-blue to-t-blue rounded-[30px]">
+          <div
+            className="flex justify-center lg:text-7xl text-6xl text-white font-Natural pt-12 "
+            data-aos="fade-down"
+          >
             Mengenal Mengwi
           </div>
           <div className="lg:inline-flex block">
-            <div className="pt-16 lg:pl-36 pl-10 max-w-[600px] lg:h-[600px] block">
+            <div
+              data-aos="fade right"
+              className="pt-16 lg:pl-36 pl-10 max-w-[600px] lg:h-[600px] block"
+            >
               <Image
                 src="/images/assets/peta-bali.png"
                 width={600}
@@ -49,14 +67,14 @@ export default function Home() {
                 alt="peta-bali"
               />
             </div>
-            <div className="block  max-w-[648px] lg:pt-24 pt-2 pl-12">
+            <div data-aos="fade-left" className="block  max-w-[648px] lg:pt-24 pt-2 pl-12">
               <p className="text-white text-lg font-poppins">
                 Kecamatan Mengwi adalah sebuah kecamatan di Kabupaten Badung,
                 Bali, Indonesia.[4] Kota Mangupura, ibukota kabupaten Badung
                 berada di kecamatan ini, yang terdiri dari beberapa desa dan
                 kelurahan di kecamatan Mengwi.
               </p>
-              <div className="flex max-w-[275px] h-[45px] lg:mt-40 mt-16 mb-4 bg-gradient-to-r from-s-orange to-t-orange items-center justify-center rounded-[30px] font-poppins text-xl text-white font-semibold">
+              <div data-aos="fade-up" className="flex max-w-[275px] h-[45px] lg:mt-40 mt-16 mb-4 bg-gradient-to-r from-s-orange to-t-orange items-center justify-center rounded-[30px] font-poppins text-xl text-white font-semibold">
                 Selengkapnya
               </div>
             </div>
@@ -66,12 +84,12 @@ export default function Home() {
       <section className="block w-full lg:h-screen h-[1600px]  bg-gradient-to-b from-[#DF693B] to-[#F99A57]">
         <div className="blockh-screen w-full bg-pattern-1">
           {/* headline */}
-          <div className="flex lg:pt-32 md:pt-48 sm:pt-80 pt-[270px]">
+          <div className="flex xl:pt-20 lg:pt-32 md:pt-48 sm:pt-80 pt-[270px]">
             <div className="mx-auto">
-              <h1 className="flex justify-center md:text-6xl text-5xl text-white font-Natural">
+              <h1 data-aos="fade-up" className="flex justify-center md:text-7xl text-6xl text-white font-Natural">
                 Desa KKN
               </h1>
-              <h1 className="flex justify-center md:text-5xl text-4xl text-white font-poppins font-bold">
+              <h1 data-aos="fade-up" className="flex justify-center md:text-5xl text-4xl text-white font-poppins font-bold">
                 Gantari-Mengwi
               </h1>
             </div>
@@ -79,8 +97,8 @@ export default function Home() {
           {/* card container */}
           <div className="flex justify-center mt-8">
             <div className="lg:flex block gap-16">
-              <HomeCard image={DesaBahaImage} desa="Baha"/>
-              <HomeCard image={DesaSobanganImage} desa="Sobangan"/>
+              <HomeCard image={DesaBahaImage} desa="Baha" anime="fade-right" />
+              <HomeCard image={DesaSobanganImage} desa="Sobangan" anime="fade-left" />
             </div>
           </div>
         </div>
